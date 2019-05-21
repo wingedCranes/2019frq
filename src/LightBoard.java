486711317643c40712cd8fabbf806e860b0df613
BT_1N3_22 <rzhao3818@bths.edu>
2019-05-21 14:22:23 -0400

    eeeeeeeeeee

--- src/LightBoard.java
+++ src/LightBoard.java
@@ -2,13 +2,21 @@ public class LightBoard {
     private boolean[][] lights;
 
     public LightBoard (int numRows, int numCol){
-
+        int chance = 0;
+       for (int a = 0; a < numRows; a++){
+           for (int b = 0; b < numCol; b++){
+            chance = (int)(Math.random() * 100 +0.1);
+            if (chance <= 40){
+                lights[a][b] = true;
+            }
+           }
+       }
     }
 
     public boolean evaluateLight (int row, int col){
         int count = 0;
        for (int i = 0; i < lights.length; i ++){
-           if (lights[row][col] == true){
+           if (lights[i][col] == true){
                count++;
            }
        }
